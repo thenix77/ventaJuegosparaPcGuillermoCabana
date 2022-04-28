366b3f53f957723dea10d2b63003a3e112802534
Utda <gcabana@senati.edu.pe>
2022-04-27 23:00:07 -0500

    20220427_2300

--- src/components/Main/Navbar.tsx
+++ src/components/Main/Navbar.tsx
@@ -5,7 +5,10 @@ export default function Navbar() {
   return (
     <Fragment>
       <nav className='navbar navbar-expand-lg navbar-dark bg-secondary '>
-        <Link to="#" className="navbar-brand">ProyectoFinal</Link>
+        <Link to="#" className="navbar-brand">
+          <img src="/images/brand.jpg" width={'30px'} alt="" />
+          Tienda Ventas Juegos
+        </Link>
         <button className='navbar-toggler' type='button' data-toggle='collapse'
                 data-target='navbarNav'
         >
@@ -18,6 +21,17 @@ export default function Navbar() {
             <NavLink to='/home/users' className="nav-item nav-link ">Users</NavLink>
             <NavLink to='/' className="nav-item nav-link ">Games</NavLink>
             <NavLink to='/home/about' className="nav-item nav-link ">about</NavLink>
+            <NavLink className="nav-item dropdown" to='#'>
+              <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Categoria
+              </Link>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><Link className="dropdown-item" to="#">Action</Link></li>
+                <li><Link className="dropdown-item" to="#">Aventura</Link></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><Link className="dropdown-item" to="#">Todos</Link></li>
+              </ul>
+            </NavLink>
           </div>
         </div>
       </nav>
